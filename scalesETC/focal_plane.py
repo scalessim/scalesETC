fixing some bugs in SNR calculation in scales.py

scalesETC/focal_plane.py
@@ -10,7 +10,7 @@ from scipy.sparse.linalg import lsmr
 
 
 class FocalPlane:
-    def __init__(self, args, gain=8):
+    def __init__(self, args, gain=11.):
 
         self.gain = gain #e/DN
         self.SkyBG = args['SkyBG']
@@ -177,7 +177,7 @@ class FocalPlane:
                     IFScube = res_sp[0].reshape([54,108,108])
                 else:
                     IFScube = res_sp[0].reshape([1900,18,17])
-
+            
             IFScube_seq[nexp] = IFScube
             if vortex == True:
                 IFScube_seq_nc[nexp] = IFScube_nc

scalesETC/scales.py
@@ -122,8 +122,18 @@ class SCALES:
              inst_emissivities = [0.4],inst_temps = [277*u.K],
              dit=1,nexps=1,extraction='optimal',
              vapor = 1.0, airmass = 1.0, change_psfs=False,
-             verbose=False,skytrans_off=False,bkg_off=False):
- 
+             verbose=False,skytrans_off=False,bkg_off=False,
+             xc=None,yc=None):
+
+        if xc==None:
+            if self.med==True:
+                yc=9
+                xc=8
+            else:
+                yc=54
+                xc=54
+            
+        
         self.skybg = SkyBG(vapor,airmass)
         self.skytrans = SkyTrans(vapor,airmass)
         if skytrans_off==True: self.skytrans.y = np.ones(self.skytrans.y.shape)*u.dimensionless_unscaled
@@ -157,18 +167,24 @@ class SCALES:
             
             img_seq,IFScube_seq = res
 
+            
+            targ_bg = Target(np.linspace(1.8,5.4,1000),np.zeros(1000))
+            img_seq_bg,IFScube_seq_bg = self.fp.get_fp(dit*u.s,nexps,self.pmat,self.rmat,Target=targ_bg,
+                                 extraction=extraction,shot_off=True,
+                                 verbose=verbose,medium=self.med,vortex=False)
 
-        elif cube!=None:        
+
+        elif cube!=None:   
             img_seq,IFScube_seq = self.fp.get_fp(dit*u.s,nexps,self.pmat,self.rmat,cube=cube,
                                                  extraction=extraction,shot_off=True,
                                                  verbose=verbose,medium=self.med)
 
         
 
-        targ_bg = Target(np.linspace(1.8,5.4,1000),np.zeros(1000))
-        img_seq_bg,IFScube_seq_bg = self.fp.get_fp(dit*u.s,nexps,self.pmat,self.rmat,Target=targ_bg,
-                                 extraction=extraction,shot_off=True,
-                                 verbose=verbose,medium=self.med,vortex=False)
+            img_seq_bg,IFScube_seq_bg = self.fp.get_fp(dit*u.s,nexps,self.pmat,self.rmat,
+                                                        cube=np.zeros(cube.shape)*u.erg/u.cm/u.cm/u.s/u.um,
+                                                        extraction=extraction,shot_off=True,
+                                                        verbose=verbose,medium=self.med,vortex=False)
 
         bkgsub_cube = IFScube_seq-IFScube_seq_bg
         SNRcube_seq = []
@@ -177,9 +193,11 @@ class SCALES:
             SNRcube = calc_SNR_cube(bkgsub_cube[nn],IFScube_seq_bg[nn])
             SNRcube_seq.append(SNRcube)
             if self.med==True:
-                SNRlist = calc_SNR_lam_ap_med(bkgsub_cube[nn],IFScube_seq_bg[nn],self.rlams)
+                SNRlist = calc_SNR_lam_ap_med(bkgsub_cube[nn],IFScube_seq_bg[nn],self.rlams,yc=yc,xc=xc)
             else:
-                SNRlist = calc_SNR_lam_ap(bkgsub_cube[nn],IFScube_seq_bg[nn],self.rlams)
-            SNRlist_seq.append(SNRlist_seq)
-        return SNRcube, SNRlist, self.rlams
+                SNRlist = calc_SNR_lam_ap(bkgsub_cube[nn],IFScube_seq_bg[nn],self.rlams,yc=yc,xc=xc)
+            SNRlist_seq.append(SNRlist)
+        SNRcube_seq = np.array(SNRcube_seq)
+        SNRlist_seq = np.array(SNRlist_seq)
+        return SNRcube_seq, SNRlist_seq, self.rlams
             

scalesETC/targs.py
@@ -238,10 +238,10 @@ def calc_SNR_cube(signal_cube,bkg_cube):
     snrcube = signal_cube/np.sqrt(signal_cube+bkg_cube)
     return snrcube
 
-def calc_SNR_lam_ap(signal_cube,bkg_cube,rlams):
+def calc_SNR_lam_ap(signal_cube,bkg_cube,rlams,yc=54,xc=54):
     xs = np.linspace(0,107,108)
     ys = np.linspace(0,107,108)
-    dists = np.array([[np.sqrt((x-54)**2+(y-54)**2) for x in xs] for y in ys])
+    dists = np.array([[np.sqrt((x-xc)**2+(y-yc)**2) for x in xs] for y in ys])
     snrs = []
     for ll in range(len(rlams)):
         lam = rlams[ll].value
@@ -253,10 +253,10 @@ def calc_SNR_lam_ap(signal_cube,bkg_cube,rlams):
     return np.array(snrs)
 
 
-def calc_SNR_lam_ap_med(signal_cube,bkg_cube,rlams):
+def calc_SNR_lam_ap_med(signal_cube,bkg_cube,rlams,yc=54,xc=54):
     xs = np.linspace(0,16,17)
     ys = np.linspace(0,17,18)
-    dists = np.array([[np.sqrt((x-8)**2+(y-9)**2) for x in xs] for y in ys])
+    dists = np.array([[np.sqrt((x-xc)**2+(y-yc)**2) for x in xs] for y in ys])
     #plt.imshow(dists)
     #plt.scatter(8,9)
     #plt.show()